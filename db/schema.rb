b7735c42c505bf09cdf5072fb2268e6a58e4190f
milenka <milenkaclaure18@gmail.com>
2015-07-24 04:04:26 +0200

    activity

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714011746) do
+ActiveRecord::Schema.define(version: 20150724010957) do
+
+  create_table "activities", force: :cascade do |t|
+    t.string   "tittle"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "text"
